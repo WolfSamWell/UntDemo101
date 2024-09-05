6b74926fe082f2794463c48998784927f30df1fb
PWG <pan.weigang@sand.com.cn>
2024-09-05 17:07:26 +0800

    Initial Commit

---
+++ DemoPriBase101/DemoPriBase101.h
@@ -0,0 +1,18 @@
+//
+//  DemoPriBase101.h
+//  DemoPriBase101
+//
+//  Created by WGPawn on 2024/9/5.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoPriBase101.
+FOUNDATION_EXPORT double DemoPriBase101VersionNumber;
+
+//! Project version string for DemoPriBase101.
+FOUNDATION_EXPORT const unsigned char DemoPriBase101VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoPriBase101/PublicHeader.h>
+
+
